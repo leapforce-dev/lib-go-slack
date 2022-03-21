1d3c016fcc02b85c110b0949ed91a82adc65f4ca
caselongo <caselongo@gmail.com>
2022-03-21 18:56:13 +0100

    first commit

---
+++ Service.go
@@ -0,0 +1,75 @@
+package dyflexis
+
+import (
+	"fmt"
+	"net/http"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	go_http "github.com/leapforce-libraries/go_http"
+)
+
+const (
+	apiName string = "Slack"
+	apiUrl  string = "https://slack.com/api"
+)
+
+// type
+//
+type Service struct {
+	apiToken    string
+	httpService *go_http.Service
+}
+
+type ServiceConfig struct {
+	ClientName string
+	ApiToken   string
+}
+
+func NewService(serviceConfig *ServiceConfig) (*Service, *errortools.Error) {
+	if serviceConfig == nil {
+		return nil, errortools.ErrorMessage("ServiceConfig must not be a nil pointer")
+	}
+
+	if serviceConfig.ApiToken == "" {
+		return nil, errortools.ErrorMessage("Service ApiToken not provided")
+	}
+
+	httpService, e := go_http.NewService(&go_http.ServiceConfig{})
+	if e != nil {
+		return nil, e
+	}
+
+	return &Service{
+		apiToken:    serviceConfig.ApiToken,
+		httpService: httpService,
+	}, nil
+}
+
+func (service *Service) httpRequest(requestConfig *go_http.RequestConfig) (*http.Request, *http.Response, *errortools.Error) {
+	// add authentication header
+	header := http.Header{}
+	header.Set("Authorization", fmt.Sprintf("Bearer %", service.apiToken))
+	(*requestConfig).NonDefaultHeaders = &header
+
+	return service.httpService.HttpRequest(requestConfig)
+}
+
+func (service *Service) url(path string) string {
+	return fmt.Sprintf("%s/%s", apiUrl, path)
+}
+
+func (service *Service) ApiName() string {
+	return apiName
+}
+
+func (service *Service) ApiKey() string {
+	return service.apiToken
+}
+
+func (service *Service) ApiCallCount() int64 {
+	return service.httpService.RequestCount()
+}
+
+func (service *Service) ApiReset() {
+	service.httpService.ResetRequestCount()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/leapforce-libraries/go_slack
+
+go 1.15
+
+require (
+	github.com/leapforce-libraries/go_errortools v0.0.0-20211119145736-7b9b61c20618
+	github.com/leapforce-libraries/go_http v0.0.0-20220220164354-4e802194f983
+	github.com/leapforce-libraries/go_oauth2 v0.0.0-20220321152643-fbcb640f7ea6
+)
