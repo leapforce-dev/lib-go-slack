ac662ebdea45e370b458eb050672c303b16911cb
caselongo <caselongo@gmail.com>
2025-03-20 22:01:51 +0100

    added wait + retry after 429

--- Service.go
+++ Service.go
@@ -3,6 +3,8 @@ package slack
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	errortools "github.com/leapforce-libraries/go_errortools"
 	go_http "github.com/leapforce-libraries/go_http"
@@ -13,8 +15,6 @@ const (
 	apiUrl  string = "https://slack.com/api"
 )
 
-// type
-//
 type Service struct {
 	apiToken    string
 	httpService *go_http.Service
@@ -50,7 +50,28 @@ func (service *Service) httpRequest(requestConfig *go_http.RequestConfig) (*http
 	header.Set("Authorization", fmt.Sprintf("Bearer %s", service.apiToken))
 	(*requestConfig).NonDefaultHeaders = &header
 
-	return service.httpService.HttpRequest(requestConfig)
+	req, res, e := service.httpService.HttpRequest(requestConfig)
+	if res.StatusCode == http.StatusTooManyRequests {
+		retryAfterSeconds := 60
+		retryAfter := res.Header.Get("Retry-After")
+		if retryAfter == "" {
+			fmt.Println("Retry-After header does not exist")
+		} else {
+			retryAfterSeconds_, err := strconv.Atoi(retryAfter)
+			if err != nil {
+				fmt.Printf("Retry-After '%s' is not a valid integer\n", retryAfter)
+			} else {
+				retryAfterSeconds = retryAfterSeconds_
+			}
+		}
+
+		fmt.Printf("Sleeping %d seconds\n", retryAfterSeconds)
+		time.Sleep(time.Duration(retryAfterSeconds) * time.Second)
+
+		return service.httpRequest(requestConfig)
+	}
+
+	return req, res, e
 }
 
 func (service *Service) url(path string) string {
